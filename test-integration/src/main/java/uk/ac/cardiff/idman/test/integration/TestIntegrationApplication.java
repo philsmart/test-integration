99d4c5ba15e27813fc701a84e734a1e20811dfbe
philsmart <philipsmart@192.168.1.4>
2016-01-23 21:40:02 +0000

    added basic integration config

--- test-integration/src/main/java/uk/ac/cardiff/idman/test/integration/TestIntegrationApplication.java
+++ test-integration/src/main/java/uk/ac/cardiff/idman/test/integration/TestIntegrationApplication.java
@@ -2,8 +2,10 @@ package uk.ac.cardiff.idman.test.integration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:integration.xml")
 public class TestIntegrationApplication {
 
 	public static void main(String[] args) {
