c289123dd8df5dd883b77f7ae770ccc5d644de7b
philsmart <philipsmart@192.168.1.4>
2016-01-23 21:24:06 +0000

    first

---
+++ test-integration/src/main/java/uk/ac/cardiff/idman/test/integration/TestIntegrationApplication.java
@@ -0,0 +1,12 @@
+package uk.ac.cardiff.idman.test.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestIntegrationApplication.class, args);
+	}
+}
---
+++ test-integration/src/test/java/uk/ac/cardiff/idman/test/integration/TestIntegrationApplicationTests.java
@@ -0,0 +1,16 @@
+package uk.ac.cardiff.idman.test.integration;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = TestIntegrationApplication.class)
+public class TestIntegrationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
